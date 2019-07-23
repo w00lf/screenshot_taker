282e4f7f0c315e6cccb1471b0a56a19aa6b1a1ba
Тараскин Михаил <MITaraskin@domclick.ru>
2019-07-23 15:51:57 +0300

    Added address field disable

--- app/assets/javascripts/scrapping_tasks.js
+++ app/assets/javascripts/scrapping_tasks.js
@@ -1,5 +1,6 @@
 $(function() {
   $('#scrapping_task_submit').click(function(e){
-    $(this).hide();
+    $(this).hide().parents('form').find('#scrapping_task_name').attr('readonly', 'readonly');
+    $('#status').text('Started parsing...');
   })
 });
