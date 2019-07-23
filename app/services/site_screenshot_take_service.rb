fb089d9b0e8568b2e869531c85fdb0ebbec2fe5a
Тараскин Михаил <MITaraskin@domclick.ru>
2019-07-23 12:40:41 +0300

    Remove droutes, refactored SiteScreenshotTakeService

--- app/services/site_screenshot_take_service.rb
+++ app/services/site_screenshot_take_service.rb
@@ -1,16 +1,18 @@
 require 'utils/watir_browser'
 
 class SiteScreenshotTakeService
+  DEFAULT_TIMEOUT = 3
+
   def self.call(url:, field_to_fill:, info_to_fill:, autocomlite_selector:, button_submit:, headless: true)
     browser = WatirBrowser.new(headless: headless).watir_browser
     browser.goto(url)
-    browser.text_field(css: field_to_fill).wait_until(&:present?).set(info_to_fill)
+    browser.text_field(css: field_to_fill).wait_until(timeout: DEFAULT_TIMEOUT, &:present?).set(info_to_fill)
     2.times do
-      browser.element(css: autocomlite_selector).wait_until(timeout: 3, &:present?).click() rescue print('Cannot click second time on elem, skipping')
-      sleep 2
+      browser.element(css: autocomlite_selector).wait_until(timeout: DEFAULT_TIMEOUT, &:present?).click() rescue print('Cannot click second time on elem, skipping')
+      sleep(DEFAULT_TIMEOUT - 1)
     end
-    browser.element(css: button_submit).wait_until(&:present?).click() if button_submit
-    sleep 3
+    browser.element(css: button_submit).wait_until(timeout: DEFAULT_TIMEOUT, &:present?).click() if button_submit
+    sleep(DEFAULT_TIMEOUT)
     screenshot_location = Rails.root.join('public', 'crawl_results')
     screenshot_file = "site_image_#{SecureRandom.uuid}.png"
     browser.screenshot.save(File.join(screenshot_location, screenshot_file))
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :scrapping_tasks
+  resources :scrapping_tasks, only: %i[create]
   mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'scrapping_tasks#index'
+  root to: 'scrapping_tasks#new'
 end
